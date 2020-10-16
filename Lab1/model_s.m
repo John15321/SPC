81ab480ba85fcbbd9eab05cc3e41b4a97ec854a9
BlueCookieFrog <35380525+BlueCookieFrog@users.noreply.github.com>
2020-10-16 21:53:48 +0200

    .

--- Lab1/model_s.m
+++ Lab1/model_s.m
@@ -5,7 +5,7 @@ b = 1;
 
 p = [1 a b];
 
-a_roots = roots(p);
+p_roots = roots(p);
 
 delta = p(2)^2 - 4*(p(1)*p(3));
 
@@ -13,3 +13,10 @@ outsim = sim('model.slx', 20);
 
 f1 = figure;
 plot(outsim.output);
+
+%a = -2, b = -1, d > 0, niestabilny
+%a = 4, b = 2, d > 0, stabilny
+%a = -2, b = 10, d < 0, niestabilny
+%a = 2, b = 4, d < 0, stabilny
+%a = -2, b = 1, d = 0, niestabilny
+%a = 2, b = 1, d = 0, stabilny
